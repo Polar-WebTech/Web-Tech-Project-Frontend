edad62e1ea514e68ed595a7241d22974bb92a078
Cheong Chien Li <clcheong00@gmail.com>
2022-07-07 21:47:30 +0800

    completed view pricing

--- admin/viewPrice.php
+++ admin/viewPrice.php
@@ -106,7 +106,7 @@ $rowProfile=mysqli_fetch_assoc($resultProfile);
                 <li ><?php //echo $row['additional']?></li>
                 <li ><?php //echo $row['additional2']?></li>
               </ul>
-              <div class="btn-wrap">
+              <div class="btn-wrap">                                                    // This is btnWrapDiv
                 <a href="#" class="btn-buy">Buy Now</a>
                 <br>
                 <br>
@@ -122,9 +122,10 @@ $rowProfile=mysqli_fetch_assoc($resultProfile);
             </div>
           </div> -->
         <?php //} ?>
-        <br>
-        <a href="InsertPrice.php">
-          <button type="button" class="editbtn" style="border-radius:25px;background-color:lightgrey;color:green;padding:10px;">Insert New Package</button></a>
+        
+        <div class="pricing" style="background-color:transparent; padding:0;">
+            <button class="box" style="margin-top:30px; background-color:transparent; border:none; padding:0;"><a class="btn-buy" href="InsertPrice.php">Insert New Package</a></button>
+        </div>
 
         </div>
 
@@ -149,36 +150,119 @@ $rowProfile=mysqli_fetch_assoc($resultProfile);
 
         for(var i=0; i<pricingArray.length; i++){
 
+          // create first div element after the row div
           var firstDiv = document.createElement("div");
           firstDiv.classList.add("col-lg-4");
           firstDiv.classList.add("col-md-6");
 
+          // create the div element with .box class
           var boxDiv = document.createElement("div");
           boxDiv.classList.add("box");
 
 
-
+          // create h3 node
           var boxDivh3 = document.createElement("h3");
           boxDivh3.innerHTML = pricingArray[i].type;
 
+          // create h4 node
           var boxDivh4 = document.createElement("h4");
-          var sup = document.createElement("sup");
-          sup.innerHTML = '$';
-          
-          var span = document.createElement("span");
-          span.innerHTML = " / per month";
+          boxDivh4.innerHTML = "<sup>$</sup>" + pricingArray[i].price + "<span> / month</span>";
 
-          var h4content = document.createElement("h4");
-          h4content.innerHTML = pricingArray[i].price;
+          // create ul node
+          var boxDivul = document.createElement("ul");
 
-          boxDivh4.appendChild(sup);
-          boxDivh4.appendChild(h4content);
-          boxDivh4.appendChild(span);
+          // create li node for basic_courses and append to ul
+          var basicCoursesli = document.createElement("li");
+          basicCoursesli.innerHTML = pricingArray[i].basic_courses;
+          boxDivul.appendChild(basicCoursesli);
+
+          // create li node for members_content and append to ul
+          var membersContentli = document.createElement("li");
+          membersContentli.innerHTML = pricingArray[i].members_content;
+          boxDivul.appendChild(membersContentli);
+
+          // create li node for practices and append to ul
+          var practicesli = document.createElement("li");
+          practicesli.innerHTML = pricingArray[i].practices;
+          boxDivul.appendChild(practicesli);
+
+          // create li node for support and append to ul
+          var supportli = document.createElement("li");
+          supportli.innerHTML = pricingArray[i].support;
+          boxDivul.appendChild(supportli);
+
+          // create li node for certification and append to ul
+          var certificationli = document.createElement("li");
+          certificationli.innerHTML = pricingArray[i].certification;
+          boxDivul.appendChild(certificationli);
+
+          // create li node for hours and append to ul
+          var hoursli = document.createElement("li");
+          hoursli.innerHTML = pricingArray[i].hours;
+          boxDivul.appendChild(hoursli);
+
+          // create li node for additional and append to ul
+          var additionalli = document.createElement("li");
+          additionalli.innerHTML = pricingArray[i].additional;
+          boxDivul.appendChild(additionalli);
+
+          // create li node for additional2 and append to ul
+          var additional2li = document.createElement("li");
+          additional2li.innerHTML = pricingArray[i].additional2;
+          boxDivul.appendChild(additional2li);
 
 
+          // create node for div after ul with .btn-wrap class
+          var btnWrapDiv = document.createElement("div");
+          btnWrapDiv.classList.add("btn-wrap");
+
+
+          // create first <a> node with .btn-buy class in the btn-wrap div, and append to btnWrapDiv
+          var a1 = document.createElement("a");
+          a1.classList.add("btn-buy");
+          a1.setAttribute("href","#");
+          a1.innerHTML = "Buy Now";
+          btnWrapDiv.appendChild(a1);
+
+
+          // add 2 <br> tags to btnWrapDiv after first <a>
+          var br1 = document.createElement("br");
+          var br2 = document.createElement("br");
+          btnWrapDiv.appendChild(br1);
+          btnWrapDiv.appendChild(br2);
+
+
+          // create second <a> node in the btn-wrap div, add href, and append to btnWrapDiv
+          var a2 = document.createElement("a");
+          a2.setAttribute("href","updateprice.php?id="+pricingArray[i].type);
+          a2.innerHTML = "Update Price";
+          btnWrapDiv.appendChild(a2);
+
+          // add 2 more <br> to btnWrapDiv
+          var br3 = document.createElement("br");
+          var br4 = document.createElement("br");
+          btnWrapDiv.appendChild(br3);
+          btnWrapDiv.appendChild(br4);
+
+
+          // create third <a> node in the btn-wrap div, add href, and append to btnWrapDiv
+          var a3 = document.createElement("a");
+          a3.setAttribute("href","deleteprice.php?id="+pricingArray[i].type);
+          a3.setAttribute("onclick","return confirm ('Are you sure to delete this price package?')");
+          a3.innerHTML = "Delete";
+          btnWrapDiv.appendChild(a3);
+
+
+          // append everything to the div with .box class
           boxDiv.appendChild(boxDivh3);
           boxDiv.appendChild(boxDivh4);
+          boxDiv.appendChild(boxDivul);
+          boxDiv.appendChild(btnWrapDiv);
+
+          // append the div with .box class to the first div after the div with .row class
           firstDiv.appendChild(boxDiv);
+
+          // append the first div to the .row div before the insert pricing button
           rowDiv.insertBefore(firstDiv,rowDiv.childNodes[i]);
 
         }
