c4d0a231d21ba34237166229bcf1dd6aba26852b
LooZhiXuen <zhixuenloo2000@gmail.com>
2022-07-14 15:37:26 +0800

    add index.php and composer.json

--- config/deletecookie.php
+++
@@ -1,6 +0,0 @@
-<?php
-include ('function.php');
-setcookie("loginuser","",time()-(3*86400),"/");
-setcookie("loginpass","",time()-(3*86400),"/");
-goto2("../login.php","Cookie has been deleted successfully");
-?>
